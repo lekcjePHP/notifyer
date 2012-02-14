c6ab0c97536a0423e4c7b66ad0ee547bd1bf145a
Art.py <lekcjephp@gmail.com>
2012-02-14 19:26:28 +0000

    dodanie i zakonczenie funkcji watcher

--- backend.py
+++ backend.py
@@ -10,3 +10,13 @@ def wyslij_mail(toaddrs,msg, username, password):
 	server.login(username,password)  
 	server.sendmail(sourceAddress, toaddrs, msg)  
 	server.quit()
+
+def watcher(pageAddress, toaddrs,msg, username, password):
+	strona = requests.get(pageAddress)
+	startSize = len(strona.text)
+	while True:
+		strona = requests.get(pageAddress)
+		actualSize = len(stronaText)
+		time.sleep(10)
+		if actualSize != startSize:
+			wyslij_mail(toaddrs, msg, username, password )
